33a660f823e2053abdc2ef9003cccf187865c29b
Jeff Kayombo <jeffkayombo16@gmail.com>
2021-08-30 07:42:16 -0700

    Completing tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *   * Return: 0 if executed properly, non-zero otherwise
+ *    */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+}
